cccbb50eb8e8bba7f070fcdb9e1b4c8bff06767e
DanilZen <a64danil@mail.ru>
2024-04-24 10:54:01 +0200

    remove useless routes from companyControlles

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -5,17 +5,10 @@ const companyService = require('../services/company/company');
 
 const routerController = require('./router/routerController')
 
-
 let router = express.Router();
 const Router = new routerController(router);
 
 Router.get('/', companyService.getCompanys)
-Router.post('/', companyService.createCompany)
-
-
 Router.get('/:id', companyService.getCompanyById);
-Router.put('/:id', companyService.updateCompanyById);
-Router.delete('/:id', companyService.deleteCompanyById);
-
 
 module.exports = router;
