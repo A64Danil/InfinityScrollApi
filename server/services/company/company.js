795e3412311e11c3c0895b2abf1b3b7400864417
DanilZen <a64danil@mail.ru>
2024-04-26 10:31:23 +0200

    add two condition - by Page and by Start, to CompanyService

--- server/services/company/company.js
+++ server/services/company/company.js
@@ -5,11 +5,31 @@ const { Company } = require('../../models/Companys/Company');
 async function getCompanys(req, res) {
     console.log('get companys')
     const query = req.query;
-    const isFullSchema = query.isFullSchema || false;
     if(query.name) {
         const Company = await getCompanyByName(req, res)
         return Company
     }
+
+    const isFullSchema = query.isFullSchema || false;
+
+    // ?page=1&limit=10
+    const page = query.page;
+    const limit = query.limit;
+
+    if(page) {
+        console.log('you are in IF-page statement')
+        const data = await Company.findAll(isFullSchema);
+        return data;
+    }
+
+    const start = query.start;
+    const end = query.end;
+    if(start >= 0) {
+        console.log('you are in IF-start statement')
+        const data = await Company.findAll(isFullSchema);
+        return data;
+    }
+
 
     const data = await Company.findAll(isFullSchema);
     return data;
